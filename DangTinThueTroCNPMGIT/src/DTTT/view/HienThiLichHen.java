580b127b3048ceb3e2420aece02567e3cf9a55ee
khanhvy-1109 <khanhmeocon98@gmail.com>
2021-06-05 21:37:35 +0700

    đưa diagram lên

--- DangTinThueTroCNPMGIT/src/DTTT/view/HienThiLichHen.java
+++ DangTinThueTroCNPMGIT/src/DTTT/view/HienThiLichHen.java
@@ -128,13 +128,13 @@ public class HienThiLichHen extends javax.swing.JPanel {
 
         jTblLichHen.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {},
+                {},
+                {},
+                {}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+
             }
         ));
         jTblLichHen.setShowGrid(false);
